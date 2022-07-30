b3078c2be68f20cd5bc4d3ddb29c971f11d950aa
chocopros <jesuschock93@gmail.com>
2022-07-30 15:05:47 -0400

    start-saturday-in-v0.00.4

--- src/components/Card__Weather.jsx
+++ src/components/Card__Weather.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import { useEffect, useState } from 'react'
 
 const Card__Weather = ({coords}) => {
-    //console.log(coords.lat)
-    //console.log(coords.lon)
-    const URL = `https://api.openweathermap.org/data/2.5/weather?lat${coords.lat}&lon${coords.lon}&appid=${APIkey}`
+    console.log(coords.lat)
+    console.log(coords.lon)
+    const lat =coords.lat
+    const lon =coords.lon
+    
+    const URL = `https://api.openweathermap.org/data/2.5/weather?lat${lat}&lon${lon}&appid=${APIkey}`
     const APIkey= '33362d39e5912619c9553d5b0a209cab'
   
 
