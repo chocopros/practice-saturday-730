bec7594774f32e010ad732fc69d568dfd8c05cc5
Anmy Moreno <nayle2701@gmail.com>
2022-07-30 14:51:47 -0400

    saturday-14:51

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+import Card__Weather from './components/Card__Weather'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  const URL = `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}`
+
+  const APIkey= '33362d39e5912619c9553d5b0a209cab'
+
+ const [coords, setCoords] = useState()
+
+ useEffect(()=>{
+
+  const success = pos =>{
+    //console.log(pos.coords.latitude)
+    //console.log(pos.coords.longitude)
+    const latlon={
+      lat: pos.coords.latitude,
+      lon: pos.coords.longitude
+    }
+    setCoords(latlon)
+  }
+  navigator.geolocation.getCurrentPosition(success)
+
+ },[])
+ 
+console.log(coords)
+
+
+
   return (
     <div className="App">
-      <h1>hola anmy</h1>
+    <Card__Weather coords={coords}/>
     </div>
   )
 }
---
+++ src/components/Card__Weather.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useEffect, useState } from 'react'
+
+const Card__Weather = ({coords}) => {
+    //console.log(coords.lat)
+    //console.log(coords.lon)
+    const URL = `https://api.openweathermap.org/data/2.5/weather?lat${coords.lat}&lon${coords.lon}&appid=${APIkey}`
+    const APIkey= '33362d39e5912619c9553d5b0a209cab'
+  
+
+  return (
+    <div>Card__Weather</div>
+  )
+}
+
+export default Card__Weather
