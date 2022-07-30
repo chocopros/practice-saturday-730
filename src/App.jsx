6597a7224358e255a956259c9f316df898281998
Anmy Moreno <nayle2701@gmail.com>
2022-07-30 15:04:54 -0400

    saturday15:04

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,7 @@ import './App.css'
 import Card__Weather from './components/Card__Weather'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  const URL = `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}`
-
-  const APIkey= '33362d39e5912619c9553d5b0a209cab'
 
  const [coords, setCoords] = useState()
 
@@ -27,8 +23,6 @@ function App() {
  },[])
  
 console.log(coords)
-
-
 
   return (
     <div className="App">
--- src/components/Card__Weather.jsx
+++ src/components/Card__Weather.jsx
@@ -4,8 +4,9 @@ import { useEffect, useState } from 'react'
 const Card__Weather = ({coords}) => {
     //console.log(coords.lat)
     //console.log(coords.lon)
-    const URL = `https://api.openweathermap.org/data/2.5/weather?lat${coords.lat}&lon${coords.lon}&appid=${APIkey}`
     const APIkey= '33362d39e5912619c9553d5b0a209cab'
+    const URL = `https://api.openweathermap.org/data/2.5/weather?lat${coords?.lat}&lon${coords?.lon}&appid=${APIkey}`
+
   
 
   return (
