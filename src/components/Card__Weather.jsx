f7fe2dbb0704d5d62abc2d7b478b60c466473609
Anmy Moreno <nayle2701@gmail.com>
2022-07-30 19:59:47 -0400

    start-saturday1959

--- src/components/Card__Weather.jsx
+++ src/components/Card__Weather.jsx
@@ -12,18 +12,15 @@ const Card__Weather = ({coords}) => {
     useEffect (() =>{
         if (coords?.lat){
             const APIkey= '33362d39e5912619c9553d5b0a209cab'            
-            const URL = `https://api.openweathermap.org/data/2.5/weather?lat${coords?.lat}&lon${coords?.lon}&appid=${APIkey}`
+            const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${coords?.lat}&lon=${coords?.lon}&appid=${APIkey}`
             axios.get(URL)
                 .then(res => setWeather(res.data))
                 .catch(err => console.log(err))
         }
 
-    },[])
+    },[coords?.lon, coords?.lat])
 
-    
-
-  
-
+    console.log(weather) 
   return (
     <div>Card__Weather</div>
   )
