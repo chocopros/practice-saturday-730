7dc7e2355e046975b4d8c15fe2ae22600da8aed8
Anmy Moreno <nayle2701@gmail.com>
2022-07-31 18:09:27 -0400

    sunday1809

--- src/assets/weathercondition.js
+++ src/assets/weathercondition.js
@@ -1,18 +1,10 @@
 const background = [ 
     {
-<<<<<<< HEAD
-      brokenclouds: "../imgweather/broken-clouds.jpg",
-      fewclouds: "../imgweather/fewclouds.jpg",
-      scatttereredcloud: "../imgweather/scatttereredcloud.jpg",
-      showerrain: "../imgweather/shower-rain.jpg",
-      snow: "../imgweather/snow.jpg"
-=======
       brokenclouds: "./imgweather/broken-clouds.webp",
       fewclouds: "./imgweather/fewclouds.jpg",
       scatttereredcloud: "./imgweather/scatttereredcloud.jpg",
       showerrain: "./imgweather/shower-rain.jpg",
       snow: "./imgweather/snow.jpg"
->>>>>>> ee1e26fb35190158de24c8f4463e5171c231ac51
 
 
     }
