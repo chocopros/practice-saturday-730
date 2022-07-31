54af4afb27daef2355d0796d983ec9e3d6cdf11d
chocopros <jesuschock93@gmail.com>
2022-07-31 00:24:03 -0400

    start-saturday-in-v0.00.11

--- src/components/Card__Weather.jsx
+++ src/components/Card__Weather.jsx
@@ -19,8 +19,8 @@ const Card__Weather = ({coords}) => {
                 .then(res => {
                   setWeather(res.data)
                   const temp = {
-                    celsius: `${(res.data.main.temp - 273.15)} °C`,
-                    farenheit:`${Math.floor((res.data.main.temp - 273.15) * 9 / 5 + 32)} °F`
+                    celsius: `${(res.data.main.temp - 273.15).toFixed(3)} °C`,
+                    farenheit:`${((res.data.main.temp - 273.15) * 9 / 5 + 32).toFixed(3)} °F`
                   }
                   setTemperture(temp) 
                   
